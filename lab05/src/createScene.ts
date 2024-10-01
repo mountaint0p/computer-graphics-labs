409a51989e1509dd7d644326e130baf93f82c3e0
Doran Penner <doranaviv@gmail.com>
2024-10-01 12:45:52 +0200

    fun formatting

--- lab05/src/createScene.ts
+++ lab05/src/createScene.ts
@@ -8,9 +8,9 @@ class Playground {
     const scene = new BABYLON.Scene(engine);
 
     // make a ArcRotateCamera, often a little easier to navigate the scene with this. It rotates around the cameraTarget.
-    const horizontalAngle = -Math.PI/2;   // initial horizontal camera angle
-    const verticalAngle = Math.PI/2;      // initial vertical camera angle
-    const distance = 20;                  // initial camera distance
+    const horizontalAngle = -Math.PI / 2;  // initial horizontal camera angle
+    const verticalAngle = Math.PI / 2;     // initial vertical camera angle
+    const distance = 20;                   // initial camera distance
     const cameraTarget = BABYLON.Vector3.Zero();
     const camera = new BABYLON.ArcRotateCamera("camera", horizontalAngle, verticalAngle, distance, cameraTarget, scene);
     camera.attachControl(canvas, true);
@@ -19,9 +19,9 @@ class Playground {
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    const box = BABYLON.MeshBuilder.CreateBox("box", {size:5}, scene);
+    const box = BABYLON.MeshBuilder.CreateBox("box", { size: 5 }, scene);
     // create ground
-    BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10}, scene);
+    BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
 
     const vertex_shader = `
         attribute vec3 position;
@@ -48,19 +48,19 @@ class Playground {
     `;
 
     // make custom ShaderMaterial for your shader code
-    const boxMaterial = new BABYLON.ShaderMaterial('myMaterial', scene, { 
-        vertexSource: vertex_shader, 
-        fragmentSource: fragment_shader
-    },{
-        attributes: ["position"], // position is BabylonJS build-in
-        uniforms: ["myWorld", "world", "view", "projection", "color"], // view, projection are BabylonJS build-in
+    const boxMaterial = new BABYLON.ShaderMaterial('myMaterial', scene, {
+      vertexSource: vertex_shader,
+      fragmentSource: fragment_shader
+    }, {
+      attributes: ["position"], // position is BabylonJS build-in
+      uniforms: ["myWorld", "world", "view", "projection", "color"], // view, projection are BabylonJS build-in
     });
 
     box.material = boxMaterial;
-  
+
     // assign color uniform
-    const boxColor = BABYLON.Vector3.FromArray([0,1,0]) // green!
-    boxMaterial.setVector3("color", boxColor); 
+    const boxColor = BABYLON.Vector3.FromArray([0, 1, 0]) // green!
+    boxMaterial.setVector3("color", boxColor);
 
     // assign custom myWorld uniform 
     const boxScalingMatrix = makeScalingMatrix(1.5, 1, 1.25);
@@ -79,27 +79,27 @@ class Playground {
     ]));
 
     function update() {
-        const time = performance.now() / 1000;
-        const zAdded = BABYLON.Matrix.FromArray([
-          // goofy fun stuff
-          Math.tan(time / 5), Math.tan(time / 5), 0, 0,
-          Math.tan(time / 5), Math.tan(time / 5), 0, 0,
-          0, 0, 0, 0,
-          0, 0, 0, 0
-        ]);
-        const trans = BABYLON.Matrix.FromArray([
-          0, 0, 0, 0,
-          0, 0, 0, 0,
-          0, 0, 0, 0,
-          Math.sin(time)*4, Math.cos(time)*5, Math.sin(time/2), 0,
-        ]);
-        boxMaterial.setMatrix("myWorld", matMult([
-          boxScalingMatrix,
-          boxZRotationMatrix.add(zAdded),
-          boxXRotationMatrix,
-          boxYRotationMatrix,
-          boxTranslationMatrix.add(trans),
-        ]));
+      const time = performance.now() / 1000;
+      const zAdded = BABYLON.Matrix.FromArray([
+        // goofy fun stuff
+        Math.tan(time / 5), Math.tan(time / 5), 0, 0,
+        Math.tan(time / 5), Math.tan(time / 5), 0, 0,
+        0,                0,                0, 0,
+        0,                0,                0, 0,
+      ]);
+      const trans = BABYLON.Matrix.FromArray([
+        0,                  0,                  0,                  0,
+        0,                  0,                  0,                  0,
+        0,                  0,                  0,                  0,
+        Math.sin(time) * 4, Math.cos(time) * 5, Math.sin(time / 2), 0,
+      ]);
+      boxMaterial.setMatrix("myWorld", matMult([
+        boxScalingMatrix,
+        boxZRotationMatrix.add(zAdded),
+        boxXRotationMatrix,
+        boxYRotationMatrix,
+        boxTranslationMatrix.add(trans),
+      ]));
     }
     scene.registerBeforeRender(update);
 
@@ -117,10 +117,10 @@ function matMult(arrs: Array<BABYLON.Matrix>) {
 
 function makeTranslationMatrix(x: number, y: number, z: number) {
   return BABYLON.Matrix.FromArray([
-    1, 0, 0, 0 ,   // <- i
-    0, 1, 0, 0 ,   // <- j
-    0, 0, 1, 0 ,   // <- k
-    x, y, z, 1 ,   // <- t
+    1, 0, 0, 0,   // <- i
+    0, 1, 0, 0,   // <- j
+    0, 0, 1, 0,   // <- k
+    x, y, z, 1,   // <- t
   ]);
 };
 
@@ -129,34 +129,34 @@ function makeScalingMatrix(x: number, y: number, z: number) {
     x, 0, 0, 0,
     0, y, 0, 0,
     0, 0, z, 0,
-    0, 0, 0, 1
+    0, 0, 0, 1,
   ])
 };
 
 function makeXRotationMatrix(angle: number) {
   return BABYLON.Matrix.FromArray([
-    1, 0, 0, 0,
+    1, 0,               0,                0,
     0, Math.cos(angle), -Math.sin(angle), 0,
-    0, Math.sin(angle), Math.cos(angle), 0,
-    0, 0, 0, 1
+    0, Math.sin(angle), Math.cos(angle),  0,
+    0, 0,               0,                1,
   ])
 };
 
 function makeYRotationMatrix(angle: number) {
   return BABYLON.Matrix.FromArray([
-    Math.cos(angle), 0, Math.sin(angle), 0,
-    0, 1, 0, 0,
+    Math.cos(angle),  0, Math.sin(angle), 0,
+    0,                1, 0,               0,
     -Math.sin(angle), 0, Math.cos(angle), 0,
-    0, 0, 0, 1
+    0,                0, 0,               1,
   ])
 };
 
 function makeZRotationMatrix(angle: number) {
   return BABYLON.Matrix.FromArray([
     Math.cos(angle), -Math.sin(angle), 0, 0,
-    Math.sin(angle), Math.cos(angle), 0, 0,
-    0, 0, 1, 0,
-    0, 0, 0, 1
+    Math.sin(angle), Math.cos(angle),  0, 0,
+    0,               0,                1, 0,
+    0,               0,                0, 1,
   ])
 };
 
