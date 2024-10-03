6172aa09a50e6ceae82fe7b73cc6bb209374ec75
mountaint0p <summit100@gmail.com>
2024-10-03 11:13:53 +0200

    Finish lab 06

--- lab06/src/createScene.ts
+++ lab06/src/createScene.ts
@@ -5,51 +5,180 @@ class Playground {
     engine: BABYLON.Engine,
     canvas: HTMLCanvasElement
   ): BABYLON.Scene {
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
-
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera(
-      'camera1',
-      new BABYLON.Vector3(0, 5, -10),
-      scene
-    );
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
+   // Scene, Camera and Light setup
+    const scene = new BABYLON.Scene(engine);
+    // Arc rotation camera allows an orbiting camera controller
+	const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, 1, 10, new BABYLON.Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
+    const light = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(-1, -1, -1), scene);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight(
-      'light1',
-      new BABYLON.Vector3(0, 1, 0),
-      scene
-    );
+    // // 'ground' mesh for reference.
+    // var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    // box mesh for use with our shader
+    var box = BABYLON.MeshBuilder.CreateBox("box", {size: 2});
+    
+    // ` ` these quatioan marks allow a multi-line string in Javascript (" " or ' ' is single line)
+    var vertex_shader = `
+        attribute vec3 position;
+        uniform mat4 myWorld;
+        uniform mat4 myView;
+        uniform mat4 myProjection;
+                
+        void main() {
+            vec4 localPosition = vec4(position, 1.);
+            vec4 worldPosition = myWorld * localPosition;     
+            vec4 viewPosition  = myView * worldPosition;
+            vec4 clipPosition  = myProjection * viewPosition;
+            gl_Position = clipPosition;
+        }
+    `;
 
-    // Our built-in 'sphere' shape. Params: name, options, scene
-    var sphere = BABYLON.MeshBuilder.CreateSphere(
-      'sphere',
-      { diameter: 2, segments: 32 },
-      scene
-    );
+    var fragment_shader = `
+        uniform vec3 color;
 
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
+        void main() {
+            gl_FragColor = vec4(color,1);
+        }
+    `;
 
-    // Our built-in 'ground' shape. Params: name, options, scene
-    var ground = BABYLON.MeshBuilder.CreateGround(
-      'ground',
-      { width: 6, height: 6 },
-      scene
-    );
+    var shaderMaterial = new BABYLON.ShaderMaterial('myMaterial', scene, { 
+        // assign source code for vertex and fragment shader (string)
+        vertexSource: vertex_shader, 
+        fragmentSource: fragment_shader
+    },
+    {
+        // assign shader inputs
+        attributes: ["position"], // position is BabylonJS build-in
+        // define custom world matrix myWorld
+        uniforms: ["myWorld", "myView", "myProjection", "color"], // view, projection are BabylonJS build-in
+    });
+    var boxColor = BABYLON.Vector3.FromArray([1,0,0]) // red
+    shaderMaterial.setVector3("color", boxColor); // assign color uniform value
 
+    let fov = Math.PI/6;
+    let aspect = 2;
+    let znear = 1;
+    let zfar = 1000;
+    let myProjection = BABYLON.Matrix.PerspectiveFovLH(fov, aspect, znear, zfar);
+    shaderMaterial.setMatrix("myProjection", myProjection);
+
+    let cameraX = 0;
+    let cameraY = 0;
+    let cameraZ = -20;
+    let cameraMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(cameraX,cameraY,cameraZ));
+    let view = cameraMatrix.invert();
+    shaderMaterial.setMatrix("myView", view);
+
+    scene.onKeyboardObservable.add((kbInfo) => {
+      switch (kbInfo.type) {
+        case BABYLON.KeyboardEventTypes.KEYDOWN:
+          switch (kbInfo.event.key) {
+            case "ArrowUp":
+              cameraZ += 1;
+              break;
+            case "ArrowDown":
+              cameraZ -= 1;
+              break;
+            case "ArrowLeft":
+              cameraX -= 1;
+              break;
+            case "ArrowRight":
+              cameraX += 1;
+              break;
+          }
+          cameraMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(cameraX,cameraY,cameraZ));
+          view = cameraMatrix.invert();
+          shaderMaterial.setMatrix("myView", view);
+        break;
+        case BABYLON.KeyboardEventTypes.KEYUP:
+        break;
+      }
+    });
+
+    
+    box.material = shaderMaterial;
+    
+    // Remember BabylonJS uses Column Major matrices
+    function makeTranslationMatrix(x, y, z){
+        var translationMatrix = 
+        [1, 0, 0, 0 ,   // <- i
+         0, 1, 0, 0 ,   // <- j
+         0, 0, 1, 0 ,   // <- k
+         x, y, z, 1 ,   // <- t
+        ];
+        return translationMatrix;
+    }
+
+    function makeRotationMatrixX(angle){
+        const cos = Math.cos(angle);
+        const sin = Math.sin(angle);
+        var rotationMatrixX = 
+        [1, 0,   0,   0,   // <- i
+         0, cos, sin, 0,   // <- j
+         0,-sin, cos, 0,   // <- k
+         0, 0,   0,   1,   // <- t
+        ];
+        return rotationMatrixX;
+    }
+
+    function makeRotationMatrixY(angle){
+        const cos = Math.cos(angle);
+        const sin = Math.sin(angle);
+        var rotationMatrixY = 
+        [cos, 0, -sin, 0,   // <- i
+         0,   1, 0,    0,   // <- j
+         sin, 0, cos,  0,   // <- k
+         0,   0, 0,    1,   // <- t
+        ];
+        return rotationMatrixY;
+    }
+
+    function makeRotationMatrixZ(angle){
+        const cos = Math.cos(angle);
+        const sin = Math.sin(angle);
+        var rotationMatrixZ = 
+        [cos,  sin, 0, 0,   // <- i
+         -sin, cos, 0, 0,   // <- j
+         0,    0,   1, 0,   // <- k
+         0,    0,   0, 1,   // <- t
+        ];
+        return rotationMatrixZ;
+    }
+
+    function makeScaleMatrix(x,y,z){
+        var scaleMatrix = 
+        [x, 0, 0, 0,   // <- i
+         0, y, 0, 0,   // <- j
+         0, 0, z, 0,   // <- k
+         0, 0, 0, 1,   // <- t
+        ];
+        return scaleMatrix;
+    }
+
+    function makeWorldMatrix(scaling, rotation, translation){
+        const translationMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(translation.x,translation.y,translation.z));
+        const rotationMatrixX = BABYLON.Matrix.FromArray(makeRotationMatrixX(rotation.x));
+        const rotationMatrixY = BABYLON.Matrix.FromArray(makeRotationMatrixY(rotation.y));
+        const rotationMatrixZ = BABYLON.Matrix.FromArray(makeRotationMatrixZ(rotation.z));
+        const scaleMatrix = BABYLON.Matrix.FromArray(makeScaleMatrix(scaling.x,scaling.y,scaling.z));
+        
+        let world = scaleMatrix.multiply(rotationMatrixZ);
+        world = world.multiply(rotationMatrixX);
+        world = world.multiply(rotationMatrixY);
+        world = world.multiply(translationMatrix);
+        return world;
+    }
+
+    
+    const scaling = new BABYLON.Vector3(1,1,1);
+    const rotation = new BABYLON.Vector3(0,0,0);
+    const translation = new BABYLON.Vector3(0,1,0);
+
+    let world = makeWorldMatrix(scaling, rotation, translation);
+    shaderMaterial.setMatrix("myWorld", world);
     return scene;
-  }
 }
+};
 
-export { Playground };
+export {Playground}
