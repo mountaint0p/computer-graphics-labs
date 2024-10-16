81bfa0c12a42b56a88fedeaaf2ca2d4f87bd7b80
Doran Penner <doranaviv@gmail.com>
2024-10-16 18:41:41 +0200

    misc formatting

--- lab06/src/createScene.ts
+++ lab06/src/createScene.ts
@@ -5,21 +5,21 @@ class Playground {
     engine: BABYLON.Engine,
     canvas: HTMLCanvasElement
   ): BABYLON.Scene {
-   // Scene, Camera and Light setup
+    // Scene, Camera and Light setup
     const scene = new BABYLON.Scene(engine);
     // Arc rotation camera allows an orbiting camera controller
-	const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, 1, 10, new BABYLON.Vector3(0, 0, 0), scene);
+    const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, 1, 10, new BABYLON.Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
     const light = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(-1, -1, -1), scene);
 
     // // 'ground' mesh for reference.
-    // var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+    // const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
     // box mesh for use with our shader
-    var box = BABYLON.MeshBuilder.CreateBox("box", {size: 2});
-    
+    const box = BABYLON.MeshBuilder.CreateBox("box", { size: 2 });
+
     // ` ` these quatioan marks allow a multi-line string in Javascript (" " or ' ' is single line)
-    var vertex_shader = `
+    const vertex_shader = `
         attribute vec3 position;
         uniform mat4 myWorld;
         uniform mat4 myView;
@@ -34,7 +34,7 @@ class Playground {
         }
     `;
 
-    var fragment_shader = `
+    const fragment_shader = `
         uniform vec3 color;
 
         void main() {
@@ -42,31 +42,31 @@ class Playground {
         }
     `;
 
-    var shaderMaterial = new BABYLON.ShaderMaterial('myMaterial', scene, { 
-        // assign source code for vertex and fragment shader (string)
-        vertexSource: vertex_shader, 
-        fragmentSource: fragment_shader
+    const shaderMaterial = new BABYLON.ShaderMaterial('myMaterial', scene, {
+      // assign source code for vertex and fragment shader (string)
+      vertexSource: vertex_shader,
+      fragmentSource: fragment_shader
     },
-    {
+      {
         // assign shader inputs
         attributes: ["position"], // position is BabylonJS build-in
         // define custom world matrix myWorld
         uniforms: ["myWorld", "myView", "myProjection", "color"], // view, projection are BabylonJS build-in
-    });
-    var boxColor = BABYLON.Vector3.FromArray([1,0,0]) // red
+      });
+    const boxColor = BABYLON.Vector3.FromArray([1, 0, 0]) // red
     shaderMaterial.setVector3("color", boxColor); // assign color uniform value
 
-    let fov = Math.PI/6;
-    let aspect = 2;
-    let znear = 1;
-    let zfar = 1000;
-    let myProjection = BABYLON.Matrix.PerspectiveFovLH(fov, aspect, znear, zfar);
+    const fov = Math.PI / 6;
+    const aspect = 2;
+    const znear = 1;
+    const zfar = 1000;
+    const myProjection = BABYLON.Matrix.PerspectiveFovLH(fov, aspect, znear, zfar);
     shaderMaterial.setMatrix("myProjection", myProjection);
 
     let cameraX = 0;
     let cameraY = 0;
     let cameraZ = -20;
-    let cameraMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(cameraX,cameraY,cameraZ));
+    let cameraMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(cameraX, cameraY, cameraZ));
     let view = cameraMatrix.invert();
     shaderMaterial.setMatrix("myView", view);
 
@@ -87,98 +87,95 @@ class Playground {
               cameraX += 1;
               break;
           }
-          cameraMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(cameraX,cameraY,cameraZ));
+          cameraMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(cameraX, cameraY, cameraZ));
           view = cameraMatrix.invert();
           shaderMaterial.setMatrix("myView", view);
-        break;
-        case BABYLON.KeyboardEventTypes.KEYUP:
-        break;
+          break;
       }
     });
 
-    
     box.material = shaderMaterial;
-    
+
     // Remember BabylonJS uses Column Major matrices
-    function makeTranslationMatrix(x, y, z){
-        var translationMatrix = 
-        [1, 0, 0, 0 ,   // <- i
-         0, 1, 0, 0 ,   // <- j
-         0, 0, 1, 0 ,   // <- k
-         x, y, z, 1 ,   // <- t
-        ];
-        return translationMatrix;
+    function makeTranslationMatrix(x: number, y: number, z: number) {
+      const translationMatrix = [
+        1, 0, 0, 0,  // <- i
+        0, 1, 0, 0,  // <- j
+        0, 0, 1, 0,  // <- k
+        x, y, z, 1,  // <- t
+      ];
+      return translationMatrix;
     }
 
-    function makeRotationMatrixX(angle){
-        const cos = Math.cos(angle);
-        const sin = Math.sin(angle);
-        var rotationMatrixX = 
-        [1, 0,   0,   0,   // <- i
-         0, cos, sin, 0,   // <- j
-         0,-sin, cos, 0,   // <- k
-         0, 0,   0,   1,   // <- t
-        ];
-        return rotationMatrixX;
+    function makeRotationMatrixX(angle: number) {
+      const cos = Math.cos(angle);
+      const sin = Math.sin(angle);
+      const rotationMatrixX = [
+        1, 0,   0,   0,  // <- i
+        0, cos, sin, 0,  // <- j
+        0,-sin, cos, 0,  // <- k
+        0, 0,   0,   1,  // <- t
+      ];
+      return rotationMatrixX;
     }
 
-    function makeRotationMatrixY(angle){
-        const cos = Math.cos(angle);
-        const sin = Math.sin(angle);
-        var rotationMatrixY = 
-        [cos, 0, -sin, 0,   // <- i
-         0,   1, 0,    0,   // <- j
-         sin, 0, cos,  0,   // <- k
-         0,   0, 0,    1,   // <- t
-        ];
-        return rotationMatrixY;
+    function makeRotationMatrixY(angle: number) {
+      const cos = Math.cos(angle);
+      const sin = Math.sin(angle);
+      const rotationMatrixY = [
+        cos, 0, -sin, 0,  // <- i
+        0,   1, 0,    0,  // <- j
+        sin, 0, cos,  0,  // <- k
+        0,   0, 0,    1,  // <- t
+      ];
+      return rotationMatrixY;
     }
 
-    function makeRotationMatrixZ(angle){
-        const cos = Math.cos(angle);
-        const sin = Math.sin(angle);
-        var rotationMatrixZ = 
-        [cos,  sin, 0, 0,   // <- i
-         -sin, cos, 0, 0,   // <- j
-         0,    0,   1, 0,   // <- k
-         0,    0,   0, 1,   // <- t
-        ];
-        return rotationMatrixZ;
+    function makeRotationMatrixZ(angle: number) {
+      const cos = Math.cos(angle);
+      const sin = Math.sin(angle);
+      const rotationMatrixZ = [
+        cos,  sin, 0, 0,  // <- i
+        -sin, cos, 0, 0,  // <- j
+        0,    0,   1, 0,  // <- k
+        0,    0,   0, 1,  // <- t
+      ];
+      return rotationMatrixZ;
     }
 
-    function makeScaleMatrix(x,y,z){
-        var scaleMatrix = 
-        [x, 0, 0, 0,   // <- i
-         0, y, 0, 0,   // <- j
-         0, 0, z, 0,   // <- k
-         0, 0, 0, 1,   // <- t
-        ];
-        return scaleMatrix;
+    function makeScaleMatrix(x: number, y: number, z: number) {
+      const scaleMatrix = [
+        x, 0, 0, 0,  // <- i
+        0, y, 0, 0,  // <- j
+        0, 0, z, 0,  // <- k
+        0, 0, 0, 1,  // <- t
+      ];
+      return scaleMatrix;
     }
 
-    function makeWorldMatrix(scaling, rotation, translation){
-        const translationMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(translation.x,translation.y,translation.z));
-        const rotationMatrixX = BABYLON.Matrix.FromArray(makeRotationMatrixX(rotation.x));
-        const rotationMatrixY = BABYLON.Matrix.FromArray(makeRotationMatrixY(rotation.y));
-        const rotationMatrixZ = BABYLON.Matrix.FromArray(makeRotationMatrixZ(rotation.z));
-        const scaleMatrix = BABYLON.Matrix.FromArray(makeScaleMatrix(scaling.x,scaling.y,scaling.z));
-        
-        let world = scaleMatrix.multiply(rotationMatrixZ);
-        world = world.multiply(rotationMatrixX);
-        world = world.multiply(rotationMatrixY);
-        world = world.multiply(translationMatrix);
-        return world;
+    function makeWorldMatrix(scaling: BABYLON.Vector3, rotation: BABYLON.Vector3, translation: BABYLON.Vector3) {
+      const translationMatrix = BABYLON.Matrix.FromArray(makeTranslationMatrix(translation.x, translation.y, translation.z));
+      const rotationMatrixX = BABYLON.Matrix.FromArray(makeRotationMatrixX(rotation.x));
+      const rotationMatrixY = BABYLON.Matrix.FromArray(makeRotationMatrixY(rotation.y));
+      const rotationMatrixZ = BABYLON.Matrix.FromArray(makeRotationMatrixZ(rotation.z));
+      const scaleMatrix = BABYLON.Matrix.FromArray(makeScaleMatrix(scaling.x, scaling.y, scaling.z));
+
+      let world = scaleMatrix.multiply(rotationMatrixZ);
+      world = world.multiply(rotationMatrixX);
+      world = world.multiply(rotationMatrixY);
+      world = world.multiply(translationMatrix);
+      return world;
     }
 
-    
-    const scaling = new BABYLON.Vector3(1,1,1);
-    const rotation = new BABYLON.Vector3(0,0,0);
-    const translation = new BABYLON.Vector3(0,1,0);
 
-    let world = makeWorldMatrix(scaling, rotation, translation);
+    const scaling = new BABYLON.Vector3(1, 1, 1);
+    const rotation = new BABYLON.Vector3(0, 0, 0);
+    const translation = new BABYLON.Vector3(0, 1, 0);
+
+    const world = makeWorldMatrix(scaling, rotation, translation);
     shaderMaterial.setMatrix("myWorld", world);
     return scene;
-}
+  }
 };
 
-export {Playground}
+export { Playground }
