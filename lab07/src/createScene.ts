9d13a4fae31612c71cf30b21da08fa784d46277e
mountaint0p <summit100@gmail.com>
2024-10-23 16:17:19 +0200

    Finish lab 07

--- lab07/src/createScene.ts
+++ lab07/src/createScene.ts
@@ -13,6 +13,8 @@ class Playground {
 		// we will use a directional light
 		const lightDirection = new BABYLON.Vector3(-0.5, -1, 0.7);
 		const light = new BABYLON.DirectionalLight("DirectionalLight", lightDirection, scene);
+		light.diffuse = new BABYLON.Color3(1, 1, 1);
+		light.specular = new BABYLON.Color3(1, 1, 1);
 
 		// 'ground' mesh for reference.
 		var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
@@ -39,6 +41,7 @@ class Playground {
 				uniform mat4 projection;
 				uniform mat3 inverseTranspose;
 
+				varying vec3 worldPos;
 				varying vec3 worldNormal;
 								
 				void main() {
@@ -50,7 +53,7 @@ class Playground {
 						// transform normal to world space, so light direction 
 						// and normal are in the same space.
 						worldNormal = inverseTranspose * normal;
-
+						worldPos = worldPosition.xyz;
 						gl_Position = clipPosition;
 				}
 		`;
@@ -59,16 +62,39 @@ class Playground {
 				uniform vec3 surfaceColor;
 				uniform vec3 lightDirection;
 				uniform float lightIntensity;
+				uniform vec3 lightColor;
+				uniform vec3 viewPosition;
+				uniform float shininess;
+				uniform float ambientTerm;
+				uniform vec3 specularColor;
 
 				varying vec3 worldNormal;
+				varying vec3 worldPos;
 
 				void main() {
 						vec3 normalizedLightDirection = normalize(lightDirection);
 						vec3 normalizedNormal = normalize(worldNormal);
+						vec3 normalizedViewDirection = normalize(viewPosition-worldPos);
 
-						// finish the algorithm to calculate simple lambertian/diffuse shading
-						vec3 pixelColor = vec3(1,0,0);
-						gl_FragColor = vec4(pixelColor,1);
+						// ambient color
+						vec3 ambientColor = ambientTerm * lightColor;
+
+						vec3 totalColor = ambientColor;
+						float cosineTheta = dot(normalizedNormal, -1.0 * normalizedLightDirection);
+						// only consider diffuse and specular when cos(theta) > 0.0
+						if (cosineTheta > 0.0) {
+							// diffuse
+							float intensity = cosineTheta * lightIntensity;
+							vec3 diffuseColor = intensity * (surfaceColor * lightColor);
+							// specular
+							vec3 halfVector = normalize(-1.0 * normalizedLightDirection + normalizedViewDirection);
+							float cosinePhi = dot(halfVector, normalizedNormal);
+							float shinyPower = pow(cosinePhi, shininess);
+							vec3 specularColorShown = shinyPower * specularColor;
+
+							totalColor = totalColor + diffuseColor + specularColorShown;
+						}
+						gl_FragColor = vec4(totalColor,1);
 				}
 		`;
 
@@ -82,7 +108,9 @@ class Playground {
 				attributes: ["position", "normal"], // position and normal are BabylonJS build-in
 				uniforms: ["world", "view", "projection", // world, view, projection are BabylonJS build-in
 					"inverseTranspose", "surfaceColor",
-					"lightDirection", "lightIntensity"]
+					"lightDirection", "lightIntensity", 
+					"viewPosition", "shininess", "ambientTerm",
+					"specularColor"]
 			});
 		var surfaceColor = BABYLON.Vector3.FromArray([1, 0, 0]) // red
 
@@ -98,6 +126,12 @@ class Playground {
 			shaderMaterial.setVector3("surfaceColor", surfaceColor);
 			shaderMaterial.setVector3("lightDirection", light.direction);
 			shaderMaterial.setFloat("lightIntensity", light.intensity);
+			shaderMaterial.setColor3("lightColor", light.diffuse);
+			shaderMaterial.setColor3("specularColor", light.specular)
+			shaderMaterial.setVector3("viewPosition", camera.position);
+			shaderMaterial.setFloat("shininess", 50.0)
+			shaderMaterial.setFloat("ambientTerm", 0.1)
+
 		}
 		scene.registerBeforeRender(update);
 
