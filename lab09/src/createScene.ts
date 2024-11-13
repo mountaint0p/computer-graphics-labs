efa47cb37e765d2bfc8d6572514c2153490ccdf7
mountaint0p <summit100@gmail.com>
2024-11-13 13:12:29 +0100

    lab 09 almost finish

--- lab09/src/createScene.ts
+++ lab09/src/createScene.ts
@@ -11,7 +11,7 @@ class Playground {
 		camera.attachControl(canvas, true);
 
 		var reflectionTexture = new BABYLON.CubeTexture("../textures/skybox2", scene);
-
+		
 		// sphere mesh for use with our shader
 		var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
 		sphere.position.y = 1;
@@ -69,6 +69,61 @@ class Playground {
 		shaderMaterial.setVector3("viewPos", camera.position);
 		shaderMaterial.setTexture("reflectionTexture", reflectionTexture)
 
+		// skybox shader
+		var skybox_vertex_shader = `
+			attribute vec3 position;
+
+			uniform mat4 view;
+			uniform mat4 projection;
+			
+			varying vec3 vPos;
+
+			void main() {
+				vec4 localPosition = vec4(position, 1.); 
+				mat4 skyboxView = view;
+
+				// remove translation data from view matrix
+				skyboxView[3].x = 0.0;
+				skyboxView[3].y = 0.0;
+				skyboxView[3].z = 0.0;
+				vec4 viewPosition  = skyboxView * localPosition;
+				vec4 clipPosition  = projection * viewPosition;
+
+				clipPosition.z = clipPosition.w; // make sure depth is maxed out
+
+				vPos = position.xyz;
+
+				gl_Position = clipPosition;
+			}
+		`;
+
+		var skybox_fragment_shader = `
+			uniform samplerCube skyboxTexture;
+
+			varying vec3 vPos;
+
+			void main() {
+				vec3 reflectionColor = textureCube(skyboxTexture, vPos).rgb;
+				gl_FragColor = vec4(reflectionColor,1);
+			}
+		`;
+
+		var skyboxShaderMaterial = new BABYLON.ShaderMaterial('skyboxMaterial', scene, { 
+			vertexSource: skybox_vertex_shader, 
+			fragmentSource: skybox_fragment_shader
+		},
+		{
+			attributes: ["position"], 
+			uniforms: ["world","view", "projection"], 
+			samplers: ["skyboxTexture"] 
+		});
+
+		skyboxShaderMaterial.setTexture("skyboxTexture", reflectionTexture);
+		skyboxShaderMaterial.disableDepthWrite = true;
+
+		// Make an inside out cube
+		var skybox = BABYLON.MeshBuilder.CreateBox("box", {size: 4, sideOrientation: BABYLON.Mesh.BACKSIDE}, scene);
+		skybox.material = skyboxShaderMaterial;
 		return scene;
 	}
 }
